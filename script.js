5dda52b189532acf68c31f1d53e41e8091acd0fb
Minskoleopc <minskoleopc@gmail.com>
2023-02-20 20:32:10 +0530

    added

--- script.js
+++ script.js
@@ -53,10 +53,6 @@ h.forEach(function (el) {
 
 //["chinmay", 123, ["pyhton", "java", "javascript"], true ,{"age":30}]
 
-
-
-
-
 // let names =["chinmay","ram","sham"]
 // let a = _.first(names)
 // console.log(a)
@@ -81,6 +77,88 @@ h.forEach(function (el) {
 // _.times(4,function(){
 //     console.log('ram')
 // })
+
+
+let e1 = _.range(10)
+console.log(e1)
+
+let e2 = _.range(1,10)
+console.log(e2)
+
+let e3 = _.range(2,22,2)
+console.log(e3)
+
+// numbers
+
+let numberss =  [33,44,-44,-66,77,88,99,100,110,900]
+console.log(numberss)
+console.log(_.max(numberss))
+console.log(_.min(numberss))
+console.log(_.min(_.range(2,20)))
+
+let obj  = [
+    {age:2},
+    {age:3},
+    {age:2},
+    {age:2}
+]
+
+console.log(_.maxBy(obj,'age'))
+console.log(_.minBy(obj,'age'))
+
+let values = [11,22,33,34,45,66,777]
+console.log(_.sum(values))
+console.log(_.sumBy(obj,'age'))
+
+
+let k = [33,44,55,66,77,88,99,55,66]
+let e4 = _.filter(k,function(el){
+    return el > 300
+})
+console.log(e4)
+
+
+let firstName = [
+    {name:"chinmay" ,age:34},
+    {name:"sarika",age:24},
+    {name:"poorva",age:14},
+    {name:"mayuri",age:24}
+]
+
+let e5 = _.find(firstName,{name:"sarika"})
+console.log(e5)
+
+let e6 = _.filter(firstName,function(el){
+    return el.age > 20
+})
+console.log(e6)
+
+
+
+let ll = [33,44,55,66,33,45,66,77,3,545,66,77,88,33,55,66]
+console.log(_.pull(ll,33,44))
+
+
+console.log(_.pull(firstName,'{age:24}'))
+
+
+
+
+
+
+
+
+
+
+// let a = [22,33,44,5555,66,77,888,921,444,55,66,77,888]
+// a.sort()
+// console.log(a)
+
+
+
+
+
+
 
 
 
